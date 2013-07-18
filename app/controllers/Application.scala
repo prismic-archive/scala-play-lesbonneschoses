57ad7e9952ef026846917d6dc110cacb36a451f7
Guillaume Bort <guillaume.bort@gmail.com>
2013-07-18 20:41:50 +0200

    Up-to-date with the current API

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,7 +8,9 @@ import com.zenexity.wroom.client._
 
 object Application extends Controller {
 
-  def SESSION = Wroom.url("http://lesbonneschoses.wroom.io/api").api()
+  def SESSION = Api.get("http://lesbonneschoses.wroom.io/api")
+
+  val PageNotFound = NotFound("OOPS")
 
   val CATEGORIES = collection.immutable.ListMap(
     "Macaron" -> "Macarons",
@@ -21,8 +23,8 @@ object Application extends Controller {
       session <- SESSION
 
       (productsRequest, featuredRequest) = (
-        session.form("documents").q("""[[$d document.type ["product"]]]""").ref(session.master).submit(),
-        session.form("documents").q("""[[$d document.tag  ["Featured"]]]""").ref(session.master).submit()
+        session.forms("products").ref(session.master).submit(),
+        session.forms("everything").query("""[[:d document.tag  "Featured"]]""").ref(session.master).submit()
       )
 
       products <- productsRequest
@@ -35,14 +37,23 @@ object Application extends Controller {
   def products = Action.async {
     for {
       session <- SESSION
-      products <- session.form("documents").q("""[[$d document.type ["product"]]]""").ref(session.master).submit()
+      products <- session.forms("products").ref(session.master).submit()
     } yield {
       Ok(views.html.products(products))
     }
   }
 
-  def product(id: String, slug: String) = Action {
-    Ok(views.html.product())
+  def product(id: String, slug: String) = Action.async {
+    for {
+      session <- SESSION
+      products <- session.forms("everything").query(s"""[[:d document.id "$id"]]""").ref(session.master).submit()
+      maybeProduct = products.headOption
+    } yield {
+      maybeProduct.collect {
+        case product if product.slug == slug => Ok(views.html.product(product))
+        case product if product.slugs.contains(slug) => MovedPermanently(routes.Application.product(id, product.slug).url)
+      }.getOrElse(PageNotFound)
+    }
   }
 
 }
