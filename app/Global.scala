d7661165035a0b3a0797ccb3dfb8830974443dfd
Guillaume Bort <guillaume.bort@gmail.com>
2013-08-26 16:51:37 +0200

    Better temp fix for PageNotFound

--- app/Global.scala
+++ app/Global.scala
@@ -2,12 +2,17 @@ import play.api._
 import play.api.mvc._
 
 import scala.concurrent._
+import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits._
+
+import controllers._
 
 object Global extends GlobalSettings {
 
   override def onHandlerNotFound(request: RequestHeader) = {
-    Results.NotImplemented("Need async result in Global.onHandlerNotFound") // FIX ME
+    Application.PageNotFound(
+      Await.result(Prismic.buildContext(request.queryString.get("ref").flatMap(_.headOption))(request), atMost = 2 seconds) // FIX ME
+    )
   }
 
 }
