09f7d6ce7db17d73b5cc051f3ba27963dff8730f
Guillaume Bort <guillaume.bort@gmail.com>
2013-08-26 19:28:57 +0200

    A few fixes

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,11 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val main = play.Project(appName, appVersion).settings(
+
+    // Prismic.io Maven repository
     resolvers += "Prismic.io kits" at "https://github.com/prismicio/repository/raw/master/maven/",
+
+    // The Java kit
     libraryDependencies += "io.prismic" %% "scala-kit" % "1.0-SNAPSHOT"
   )
 
