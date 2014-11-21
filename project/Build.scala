7bcc2f8373f9fb5fbb644ac75d63deee69af85f3
Erwan Loisant <eloisant@gmail.com>
2014-11-21 15:38:10 +0100

    Update kit version

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object ApplicationBuild extends Build {
     scalaVersion := "2.11.1",
 
     // The Scala kit
-    libraryDependencies += "io.prismic" %% "scala-kit" % "1.2.9"
+    libraryDependencies += "io.prismic" %% "scala-kit" % "1.2.12"
   )
 
 }
